530f72e622556b981c8be9ee3368d0ddfaadc6f3
rinat <atkbrc354@mail.ru>
2023-03-26 12:40:17 +0300

    init: reset --hard

---
+++ flowers/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from './components/Catalog/Header';
+import "./sass/app.sass";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
