9032bf4e8f29e371edd2d5469ba203cf428a4019
rinat <atkbrc354@mail.ru>
2023-03-28 12:23:24 +0300

    feah: header-menu

--- flowers/src/components/Catalog/Header.jsx
+++ flowers/src/components/Catalog/Header.jsx
@@ -6,13 +6,21 @@ import style from './Header.module.scss';
 export const Header = () => {
     return (
         <header className={style.root}>
-            <ul className={style.list}>
-                <li><a href="#" className={style.l}>Каталог</a></li>
-                <li><a href="#" className={style.l}>Доставка и оплата</a></li>
-                <li><a href="#" className={style.l}>О нас</a></li>
-                <li><a href="#" className={style.l}>Контакты</a></li>
-                <li><a href="#" className={style.l}>FAQ</a></li>
-            </ul>
+            <div className="container">
+                <div className="wrapper">
+                    <div className={style.header_block}>
+                        <nav>
+                            <ul className={style.list}>
+                                <li><a href="#" className={style.l}>Каталог</a></li>
+                                <li><a href="#" className={style.l}>Доставка и оплата</a></li>
+                                <li><a href="#" className={style.l}>О нас</a></li>
+                                <li><a href="#" className={style.l}>Контакты</a></li>
+                                <li><a href="#" className={style.l}>FAQ</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
